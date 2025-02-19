34253d29520bf10216cac09e9ebbd4e64a66a38d
Vitalii Kobyliak <vitalii.kobyliak@spryker.com>
2025-02-19 12:54:44 +0200

    SLA-7450: Fixed constraint.

--- src/SprykerShop/Yves/CustomerPage/Form/GuestForm.php
+++ src/SprykerShop/Yves/CustomerPage/Form/GuestForm.php
@@ -164,7 +164,9 @@ class GuestForm extends AbstractType
                 new Callback([
                     'callback' => function ($email, ExecutionContextInterface $context) {
                         if (!$email) {
-                            return $this->createNotBlankConstraint();
+                            $context->buildViolation(static::VALIDATION_NOT_BLANK_MESSAGE)->addViolation();
+
+                            return;
                         }
                         $isEmailFormatValid = $this->getFactory()
                             ->getUtilValidateService()
